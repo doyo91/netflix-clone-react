df3182c1ec2e11b633774999b7c65eb241d59959
doyo91 <doyo_91@hotmail.com>
2020-10-01 10:32:44 +0200

    Add Header

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,32 @@
 import React from "react";
-
+import { Feature, OptForm } from "../components";
 import JumbotronContainer from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
 import { FaqsContainer } from "../containers/faqs";
+import { HeaderContainer } from "../containers/header";
 
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Todas las películas y series que desees, y mucho más.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Disfruta donde quieras. Cancela cuando quieras.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Text>
+              ¿Quieres ver algo ya? Escribe tu correo para crear una suscripción
+              a Netflix o reactivarla.
+            </OptForm.Text>
+            <OptForm.Break />
+            <OptForm.Input placeholder="Correo electrónico" />
+            <OptForm.Button>Empezar</OptForm.Button>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
