5810a8707792c9dba7ab493e681abde0f1cd0421
doyo91 <doyo_91@hotmail.com>
2020-10-06 12:26:55 +0200

    Add firebase and seed

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,18 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: "proyectos-react",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
